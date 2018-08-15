2bd78799abaebc24bf277762cb44860683211165
Thomas Boerger <thomas@webhippie.de>
2018-08-15 20:05:35 +0200

    Filter versions by version constraints

---
+++ examples/filter/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kleister/go-forge/version"
+)
+
+func main() {
+	fmt.Println("Fetching Forge versions...")
+	forge, err := version.FromDefault()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	f := &version.Filter{
+		Version: ">=13.20,<14.23.4",
+	}
+
+	for _, version := range forge.Releases.Filter(f) {
+		fmt.Println(version.ID)
+	}
+}
